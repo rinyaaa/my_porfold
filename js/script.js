d2c94eef7f843f3a7ffd0649fd830f997f4163fb
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2024-05-30 00:03:14 +0900

    星の色を変えたよ

--- js/script.js
+++ js/script.js
@@ -3,3 +3,5 @@
 const userInput ="こんにちは!始めまして。石丸凜弥と申します 。\n2005年7月18日生まれ長崎出身です。 　　　　　\n愛知工業大学の情報学生です。　 　　 　　　　\n高校も情報でしたがコードは全然書けないです。\nこれから頑張るのでよろしくお願いします　　　"
 
 document.getElementById('user-text').innerText = userInput;
+
+  
